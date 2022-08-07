4d96fd4ccc655498b236f1db062085939cb001f1
Julian Sniffen <julian.sniffen@gmail.com>
2022-08-06 22:46:32 -0400

    updates

--- src/file_buffer.c
+++ src/file_buffer.c
@@ -3,15 +3,16 @@ typedef struct {
 	char look;
 	int index;
 	int length;
-	write_buffer wb;
 } file_buffer;
+
+char write_buffer[2048];
+int write_index = 0;
 
 void fb_init(file_buffer *fb, char *filename)
 {
 	read_file(filename, &fb->data, &fb->length);
 	fb->index = 0;
 	fb->look = fb->data[0];
-	wb_init(&fb->wb);
 }
 
 void fb_next(file_buffer *fb)
@@ -53,10 +54,11 @@ void fb_function(file_buffer *fb)
 
 void fb_parse(file_buffer *fb)
 {
-	wb_write(&fb->wb, "writing\n", 8);
+	int len = sprintf(write_buffer+write_index, "writing v2\n");
+	write_index += len;
 }
 
 void fb_write(file_buffer *fb, char *filename)
 {
-	write_file(filename, fb->wb.data, fb->wb.length);
+	write_file(filename, write_buffer, write_index);
 }
