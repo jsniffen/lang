dae04f1607c0d572b86f29ba593562dbdd9d7540
Julian Sniffen <julian.sniffen@gmail.com>
2022-08-08 21:25:18 -0400

    add windows asm

---
+++ asm/hello_windows.asm
@@ -0,0 +1,14 @@
+; nasm -fwin64 hello_windows.asm
+; link /ENTRY:main hello_windows.obj ucrt.lib
+; hello_windows
+        global  main
+        extern  puts
+        section .text
+main:
+        sub     rsp, 28h                        ; Reserve the shadow space
+        mov     rcx, message                    ; First argument is address of message
+        call    puts                            ; puts(message)
+        add     rsp, 28h                        ; Remove shadow space
+        ret
+message:
+        db      'Hello', 0                      ; C strings need a zero byte at the end
